0c8804a7a6b93841d0d6f086805a567133c91644
Rostislav <rstk.tischenko@gmail.com>
2025-02-01 16:22:37 +0200

    Delete token and add "Token test" output

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Test app 2");
         System.out.println("Yo");
+        System.out.println("Token test");
     }
 }
